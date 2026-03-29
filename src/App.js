dde0f0c8441b02a0010369a8865bda44ca681d23
prabh293 <prabhjot.singh@vinisoft.org>
2026-03-29 07:04:51 +0530

    my name header

--- src/App.js
+++ src/App.js
@@ -68,7 +68,10 @@ function App() {
           <button onClick={joinRoom}>Join A Room</button>
         </div>
       ) : (
+        <>
+         <h1>Welcome to Prabhjot Chat App</h1>
         <Chat socket={socket} username={username} room={room} />
+        </>
       )}
     </div>
   );
